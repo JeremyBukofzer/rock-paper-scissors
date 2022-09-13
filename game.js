ed101ac50b17b3f7d7e1b0ea7f3fa6f51a1f72d3
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-09-12 19:38:36 -0500

    pseudo code

--- game.js
+++ game.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+// define array for choices [r,p,s]
+// define var for computerChoice
+// define var for userChoice
+// define var for wins
+// define var for losses
+// define var for ties
+
+// prompt userChoice
+
+// randomly choose computerChoice
+
+// Compare choices
+    
+
+// display (alert) results (win, tie, lose round)
+
+// show stats (number of wins, losses, and ties)
+
+// play again
+    //restart game
+// else
+    // end game
+
